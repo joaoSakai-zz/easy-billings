4024a82c3216aed969e9b7b15eb4411b95757ebb
Felipe Burin <fburin@ciandt.com>
2018-02-24 13:34:15 -0300

    Spring boot first configuration

---
+++ src/main/java/com/joaosakai/EasyBillings/EasyBillingsApplication.java
@@ -0,0 +1,12 @@
+package com.joaosakai.EasyBillings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBillingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBillingsApplication.class, args);
+	}
+}
